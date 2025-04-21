e36d60a403983df0503b135b1b03ab74a6526be3
Trantien181 <tranmanhtien181@gmail.com>
2025-04-21 16:25:10 +0700

    cap nhat controller, thymleaf lan 2

--- src/main/java/com/example/WebsiteBanHang2/Repository/SanPhamChiTietRepository.java
+++ src/main/java/com/example/WebsiteBanHang2/Repository/SanPhamChiTietRepository.java
@@ -3,6 +3,22 @@ package com.example.WebsiteBanHang2.Repository;
 import com.example.WebsiteBanHang2.Dto.SanPhamChiTietDTO;
 import com.example.WebsiteBanHang2.Model.SanPhamChiTiet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SanPhamChiTietRepository extends JpaRepository<SanPhamChiTiet, Integer> {
+    @Query("""
+        select new com.example.WebsiteBanHang2.Dto.KichThuocDTO(
+            kt.id,
+            kt.maKichThuoc,
+            kt.tenKichThuoc,
+            kt.trangThai
+        ) from SanPhamChiTiet spct 
+        join KichThuoc kt 
+        on spct.kichThuocId.id = kt.id
+        where spct.kichThuocId=?1
+        
+""")
+    public List<SanPhamChiTietDTO> getKichThuoc(Integer id);
 }
